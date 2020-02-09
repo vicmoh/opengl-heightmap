c193f6643a579ad219e8791bec62b161b5407cc4
Vicky Mohammad <mohammav@sysadmins-iMac.local>
2020-02-08 21:41:03 -0500

    [AUTO]

--- include/point.h
+++ include/point.h
@@ -4,12 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "array_map.h"
+#include "dynamic_string.h"
 
 typedef struct {
   long double x, y, z;
+  String toString;
 } Point;
 
 Point* new_Point(long double x, long double y, long double z);
+void free_Point(Point* this);
 Array* getSphereVertices(double r, double lats, double longs);
 
 #endif
